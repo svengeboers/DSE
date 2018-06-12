7f8d89f9d56cdee57d230a9bf0e9187d17ad7192
br33213 <geboers.sven@gmail.com>
2018-06-12 15:01:49 +0200

    Last commit. Change matrix if you still want to do anything with it. Right now it is overconstrained.

--- MatrixMethodStructures.m
+++ MatrixMethodStructures.m
@@ -10,6 +10,22 @@
 % - gravity equally distributed over two supports
 % - Neglect shear deformation
 
+airfoil.filename = 'NACA2321.txt';     %Airfoil file
+airfoil.t = 4*10^-3;                 	%Thickness in m
+airfoil.C = 1.1;                      	%Chord length in m
+theta_p = 0;                            %pitch angle 
+
+airfoil.spar1 = true;                       	%spar 1 option
+airfoil.s1t = 4*10^-3;                       	%spar thickness in m
+airfoil.s1c = 0.2;                            	%cordwise position 
+
+airfoil.spar2 = true;                           %spar 2 option
+airfoil.s2t = 4*10^-3;                        	%spar thickness
+airfoil.s2c = 0.5;                          	%cordwise position 
+
+
+[In,Ib,~,~,~,~] = airfoilInertia(airfoil,0,0);
+It = In+Ib; % Polar moment of inertia
 load('/home/sgeboers/Dropbox/DSE/DSE/QX.mat')
 load('/home/sgeboers/Dropbox/DSE/DSE/QY.mat')
 load('/home/sgeboers/Dropbox/DSE/DSE/S.csv')
@@ -38,8 +54,8 @@ syms s
 sb = 0; 
 s1 = 0; %Placeholder values for s1 and s2
 s2 = 0; 
-qx = 0*s+300; %Placeholder values
-qy = 0*s+50;
+qx = 0*s+50; %Placeholder values
+qy = 0*s+5;
 qz = 0*s+9.80665; %still has to be multiplied by mass per area
 
 % Definite integrals for previously mentioned expressions
@@ -50,16 +66,14 @@ int(qz);
 %% Defining constants for THE MATRIX
 a = 15; %this is the radius of the helix
 H = 45; %Height of the rotor
-b = H*3/pi; %Constant that defines the helix
+b = H*pi/3; %Constant that defines the helix
 c = sqrt(a^2+b^2); %Constant that defines the helix
-S = sb:0.1:st; %Array which will be used to plot things along the length of the beam later on
-st = c*pi/3; %Length of the curve
-E = 1000;
-A = 1700; %Not necessarily needed, only for shear deformation which we ignore
-G = 3000;
-It = 2000;
-In = 1500;
-Ib = 2100;
+st = c*pi/3; %Length of the curve [m]
+ds = 1; %s step [m]
+S = sb:ds:st; %Array which will be used to plot things along the length of the beam later on
+E = 70*10^9; %23*10^9; %Youngs modulus (Pa) 3.7 GPA might also be correct
+A = 1000; %Not necessarily needed, only for shear deformation which we ignore
+G = 30*10^9; %1.9*10^9; %Torsional modulus
 
 
 % Transformation matrix
@@ -89,19 +103,50 @@ yxz = -a*b*sin(s/c)/(c^2*G*It)+a*b*sin(s/c)/(c^2*E*Ib);
 yyz = a*b*cos(s/c)/(c^2*G*It)-a*b*cos(s/c)/(c^2*E*Ib);
 
 % Reaction forces
-Fxb = 200;
+x = a*cos(S/c);
+y = a*sin(S/c);
+z = b*S/c;
+
+qX = subs(qx,s,S);
+qY = subs(qy,s,S);
+qZ = subs(qz,s,S);
+
+
+Sumx = - sum(double(-qY.*[0,diff(S)].*z)+double(qZ.*[0,diff(S)].*y));
+Sumy = - sum(double(-qZ.*[0,diff(S)].*x)+double(qX.*[0,diff(S)].*z));
+Sumz = - sum(double(-qX.*[0,diff(S)].*y)+double(qY.*[0,diff(S)].*x));
+
+syms Fxb Fyb Fzb Fxt Fyt Fzt
+
+eqn1 = - Sumx + Fzb*double(a*sin(sb/c))-Fyb*double(sb*b/c)+Fzb*double(a*sin(st/c))-Fyt*double(st*b/c)==0;
+eqn2 = - Sumy + Fxb*double(sb*b/c)-Fzb*double(a*cos(sb/c))+Fxb*double(st*b/c)-Fzt*double(a*cos(st/c))==0;
+eqn3 = - Sumz + Fyb*double(a*cos(sb/c))-Fxb*double(a*sin(sb/c))+Fyb*double(a*cos(st/c))-Fxt*double(a*sin(st/c))==0;
+
+eqn4 = - sum(double(qX.*[0,diff(S)])) + Fxb + Fxt==0;
+eqn5 = - sum(double(qY.*[0,diff(S)])) + Fyb + Fyt==0;
+eqn6 = - sum(double(qZ.*[0,diff(S)])) + Fzb + Fzt==0;
+
+[A,B] = equationsToMatrix([eqn1 eqn2 eqn3 eqn4 eqn5 eqn6],[Fxb Fyb Fzb Fxt Fyt Fzt]);
+
+X = double(linsolve(A,B));
+
+%Calculation of reaction forces
+Fxb = X(1);
+Fyb = X(2);
+Fzb = X(3);
+Fxt = X(4);
+Fyt = X(5);
+Fzt = X(6);
+
+
+
+
 Fx1 = 0;
 Fx2 = 0;
-Fxt = 500;
-Fyb = 300;
 Fy1 = 0;
 Fy2 = 0;
-Fyt = 600;
-Fzb = qz*st/2;
 Fz1 = 0; %Assumption is made that the gravity load is distributed 
 Fz2 = 0; %equally among the two supports (b and t)
-Fzt = qz*st/2;
-
 syms c1 c2 c3 c4 c5 c6 c7 c8 c9 c10 c11 c12
 
 Vx = Fxb + 0*Fx1*heaviside(s-s1) + 0*Fx2*heaviside(s-s2) + 0*Fxt*heaviside(s-st) - qx*s + c1;
@@ -114,14 +159,14 @@ My = int(vtx*Vz-vtz*Vx) + c5;
 Mz = int(vty*Vx-vtx*Vy) + c6;
 
 % Angular displacements
-thetax = int(yxx*Mx+yxy*My+yxz*Mz) + c7;
-thetay = int(yxy*Mx+yyy*My+yyz*Mz) + c8;
-thetaz = int(yxz*Mx+yyz*My+yzz*Mz) + c9;
+%thetax = int(yxx*Mx+yxy*My+yxz*Mz) + c7;
+%thetay = int(yxy*Mx+yyy*My+yyz*Mz) + c8;
+%thetaz = int(yxz*Mx+yyz*My+yzz*Mz) + c9;
 
 % Displacements
-deltax = vtz*thetay-vty*thetaz + c10;
-deltay = vtx*thetaz-vtz*thetax + c11;
-deltaz = vty*thetax-vtx*thetay + c12;
+deltax = int(vtz*thetay-vty*thetaz) + c7*s + c10;
+deltay = int(vtx*thetaz-vtz*thetax) + c8*s + c11;
+deltaz = int(vty*thetax-vtx*thetay) + c9*s + c12;
 
 % Calculation of constant c1, c2 and c3
 % Shear force at the end should equal the reaction force
@@ -131,7 +176,6 @@ eqn2 = subs(Vy,s,st)==-Fyt;
 eqn3 = subs(Vz,s,st)==-Fzt;
 
 % Calculation of constant c4, c5 and c6
-% Moments yet to be obtained from Louis K
 
 eqn4 = subs(Mx,s,st)==0;
 eqn5 = subs(My,s,st)==0;
@@ -140,12 +184,12 @@ eqn6 = subs(Mz,s,st)==0;
 % Calculation of constant c7, c8 and c9
 % angular displacements at the end should be 0
 
-eqn7 = subs(thetax,s,st)==0;
-eqn8 = subs(thetay,s,st)==0;
-eqn9 = subs(thetaz,s,st)==0;
+eqn7 = subs(deltax,s,sb)==0;
+eqn8 = subs(deltay,s,sb)==0;
+eqn9 = subs(deltaz,s,sb)==0;
 
 % Calculation of constant c10, c11 and c12
-% Displacement at the end should be 0 (clamped)
+% Displacement at the end should be 0 (hinged)
 eqn10 = subs(deltax,s,st)==0;
 eqn11 = subs(deltay,s,st)==0;
 eqn12 = subs(deltaz,s,st)==0;
@@ -169,15 +213,14 @@ My = int(vtx*Vz-vtz*Vx) + X(5);
 Mz = int(vty*Vx-vtx*Vy) + X(6);
 
 % Angular displacements
-thetax = int(yxx*Mx+yxy*My+yxz*Mz) + X(7);
-thetay = int(yxy*Mx+yyy*My+yyz*Mz) + X(8);
-thetaz = int(yxz*Mx+yyz*My+yzz*Mz) + X(9);
+%thetax = int(yxx*Mx+yxy*My+yxz*Mz) + X(7);
+%thetay = int(yxy*Mx+yyy*My+yyz*Mz) + X(8);
+%thetaz = int(yxz*Mx+yyz*My+yzz*Mz) + X(9);
 
 % Displacements
-deltax = vtz*thetay-vty*thetaz + X(10);
-deltay = vtx*thetaz-vtz*thetax + X(11);
-deltaz = vty*thetax-vtx*thetay + X(12);
-
+deltax = int(vtz*thetay-vty*thetaz) + X(7)*s + X(10);
+deltay = int(vtx*thetaz-vtz*thetax) + X(8)*s + X(11);
+deltaz = int(vty*thetax-vtx*thetay) + X(9)*s + X(12);
 
 Vx = double(subs(Vx,s,S));
 Vy = double(subs(Vy,s,S));
@@ -198,8 +241,11 @@ deltaz = double(subs(deltaz,s,S));
 % All values can now be plotted against S
 % For example plot(S,Vx)
 
+plot(S,deltax)
 
 
+
+%plot(S,x)
 
 
 
